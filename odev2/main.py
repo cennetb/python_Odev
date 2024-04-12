dce97412f3adc881b80b8505747e67722c22d59b
cennetb <119862319+cennetb@users.noreply.github.com>
2024-04-12 19:59:26 +0300

    Add files via upload

---
+++ odev2/main.py
@@ -0,0 +1,8 @@
+import my_module
+
+if __name__ == "__main__":
+
+    print(my_module.is_letter("a"))
+    print(my_module.convert_to_lowercase("A"))
+    print(my_module.calculate_letter_frequency('Alinin mutfağa getirdiği sepet sarıydı.'))
+    my_module.display_info()
